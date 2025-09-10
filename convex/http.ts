df77a8e9f4258a5df70094bc229e7661f08c62c7
debkanta1020 <mhc2024003@iiita.ac.in>
2025-09-10 05:39:50 +0530

    Update http.ts

--- convex/http.ts
+++ convex/http.ts
@@ -7,7 +7,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const http = httpRouter();
 
-const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY!);
+const genAI = new GoogleGenerativeAI(process.env.OPENAI_API_KEY!);
 
 http.route({
   path: "/clerk-webhook",
